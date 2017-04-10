dbf5ef7448bb091d50b82076e2e5d5b1dca7377e
Eliminater74 <eliminater74@gmail.com>
2017-04-10 09:37:56 -0400

    minor changes
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- Android.mk
+++ Android.mk
@@ -4,16 +4,20 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_STATIC_JAVA_LIBRARIES := android-support-v13
-LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
-LOCAL_STATIC_JAVA_LIBRARIES += xmp_toolkit
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    android-support-v13        \
+    android-support-v4         \
+    glide                      \
+    xmp_toolkit
 
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-LOCAL_SRC_FILES += $(call all-java-files-under, src_pd)
-LOCAL_SRC_FILES += $(call all-java-files-under, src_pd_gcam)
-LOCAL_SRC_FILES += $(call all-renderscript-files-under, rs)
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, src) \
+    $(call all-java-files-under, src_pd) \
+    $(call all-java-files-under, src_pd_gcam) \
+    $(call all-renderscript-files-under, rs) \
 
-LOCAL_RESOURCE_DIR += $(LOCAL_PATH)/res
+LOCAL_RESOURCE_DIR := \
+    $(LOCAL_PATH)/res
 
 include $(LOCAL_PATH)/version.mk
 LOCAL_AAPT_FLAGS := \
@@ -21,7 +25,7 @@ LOCAL_AAPT_FLAGS := \
         --version-name "$(version_name_package)" \
         --version-code $(version_code_package) \
 
-LOCAL_PACKAGE_NAME := SnapdragonCamera
+LOCAL_PACKAGE_NAME := Snap
 LOCAL_PRIVILEGED_MODULE := true
 
 #LOCAL_SDK_VERSION := current
